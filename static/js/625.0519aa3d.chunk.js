655716558b4d4306a5366fa8670998cbf4845b90
Ayushman Datta <ayushmandatta@gmail.com>
2024-04-22 03:28:57 +0530

    Updates

--- static/js/625.0519aa3d.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[625],{625:(o,t,e)=>{o.exports=e.p+"static/media/about.078b0cda8131dc4113ff.md"}}]);
---
+++ static/js/625.da359c06.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpersonal_site=self.webpackChunkpersonal_site||[]).push([[625],{625:(e,s,a)=>{e.exports=a.p+"static/media/about.078b0cda8131dc4113ff.md"}}]);
